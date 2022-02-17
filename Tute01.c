9be7c93a36e4f16805200e495319a05226e1239a
Pallage P.D.R.S <99821189+IT21311604@users.noreply.github.com>
2022-02-17 14:54:01 +0000

    #2 Completed

--- Tute01.c
+++ Tute01.c
@@ -9,14 +9,14 @@ int main() {
   int mark1, mark2; //declaring variables
   float avg;
 
-  printf("Enter Mark 1 : "); 
+  printf("Enter Mark 1 :: "); 
   scanf("%d", &mark1); //getting user input
-  printf("Enter Mark 2 : ");
+  printf("Enter Mark 2 :: ");
   scanf("%d", &mark2);
 
   avg = (mark1 + mark2) / 2.0; //calculate the average
 
-  printf("Average : %2.f", avg);
+  printf("Average :: %2.f", avg);
   
   return 0;
 }
